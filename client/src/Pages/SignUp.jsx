d1a73542a6a19916a17c60b668ee2b0a62da6534
Vasanth N <vasanthnimmala69@outlook.com>
2023-12-11 22:39:40 +0530

    Client side errors at Signup page

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,5 +1,35 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function SignUp() {
+  const [formData, setForm] = useState([]);
+  const handleChange = (e) => {
+    setForm({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const responce = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then(() => {
+          console.log("Sent");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      const data = await responce.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="max-w-lg p-3 mx-auto min-w-max">
       <div className="font-mono text-3xl text-center my-7">Signup</div>
@@ -7,20 +37,30 @@ export default function SignUp() {
         <input
           className="justify-center p-2 rounded-md bg-slate-100"
           placeholder="Username"
+          id="username"
+          onChange={handleChange}
+          required
         ></input>
         <input
           className="p-2 rounded-md bg-slate-100"
           placeholder="Email"
           type="email"
+          id="email"
+          onChange={handleChange}
+          required
         ></input>
         <input
           className="p-2 rounded-md bg-slate-100"
           placeholder="Password"
           type="password"
+          id="password"
+          onChange={handleChange}
+          required
         ></input>
         <button
           className="p-2 text-gray-100 uppercase rounded-md bg-slate-800 hover:bg-slate-900"
           type="submit"
+          onSubmit={handleSubmit}
         >
           Signup
         </button>
@@ -30,7 +70,7 @@ export default function SignUp() {
       </form>
       <div className="flex gap-2">
         <p>Have an account?</p>
-        <Link to={"/signup"}>
+        <Link to={"/signin"}>
           <span className="text-cyan-700">signin</span>
         </Link>
       </div>
